9b4badb378d4f3ee9b0e3f2a2f9d1e95bdc39ead
Rim <121716030+Rim1Belabadia1@users.noreply.github.com>
2024-12-29 12:59:03 +0100

    correction

--- emp_backend/src/main/java/com/example/demo/repository/EmpruntRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Emprunt;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Transactional
-public interface EmpruntRepository extends JpaRepository<Emprunt, Long> {
-}
--- employee frontend final/src/app/models/emprunt.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Emprunt } from './emprunt.model';
-
-describe('Emprunt', () => {
-  it('should create an instance', () => {
-    expect(new Emprunt()).toBeTruthy();
-  });
-});
